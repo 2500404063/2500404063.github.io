9fe128f815b5a7a6f08d1373158df9412ad81956
Felix <859317053@qq.com>
2021-06-24 08:09:13 +0800

    Add files via upload

--- js/index_js.js
+++ js/index_js.js
@@ -1,13 +1,29 @@
 function loaded() {
-    setTimeout(function(){
+    setTimeout(function () {
         document.querySelector("#loading").style.display = "none"
-    },1000);
+    }, 1000);
 }
 
-function btn_myself(){
-    window.open("./myself/index.html","_blank")
+function btn_myself() {
+    window.open("./myself/index.html", "_self")
 }
 
-function btn_webdev(){
-    window.open("./webdev/index.html","_blank")
+function btn_webdev() {
+    window.open("./webdev/index.html", "_self")
+}
+
+function btn_appdev() {
+    window.open("./appdev/index.html", "_self")
+}
+
+function btn_hardware() {
+    window.open("./hardware/index.html", "_self")
+}
+
+function btn_ai() {
+    window.open("./ai/index.html", "_self")
+}
+
+function btn_psyeco() {
+    window.open("./psyeco/index.html", "_self")
 }
