8ebf987e89a443838edd07753d10b21e54834a7f
Felix <859317053@qq.com>
2021-06-27 08:25:40 +0800

    Add files via upload

--- blocks/pages_psyeco/contentStructure.js
+++ blocks/pages_psyeco/contentStructure.js
@@ -1,16 +1,18 @@
 var contentArray = {
     "Default Page": "./pages_psyeco/default/default.md",
     "生活哲学": {
+        "为什么要哲学?": "./pages_psyeco/life-psy/1.md",
         "心理学": {},
         "论性格": {},
         "论友情": {},
         "论爱情": {},
         "论事业": {
-            "工作-心态-健康": "./pages_psyeco/life-psy/cause/1.md"
+            "工作-心态-健康": "./pages_psyeco/life-psy/cause/1.md",
+            "过度思考Overthink": "./pages_psyeco/life-psy/cause/2.md",
         },
         "论学习": {}
     },
-    "经济学":{},
-    "博弈论":{},
-    "塔罗牌":{}
+    "经济学": {},
+    "博弈论": {},
+    "塔罗牌": {}
 };
