50a7bcc1818bf0f0bd1e28fe6c515a2801ab1d47
Felix <859317053@qq.com>
2021-06-30 22:09:52 +0800

    Add files via upload

--- blocks/js/contentIndex_controller.js
+++ blocks/js/contentIndex_controller.js
@@ -166,10 +166,10 @@ function getMathJaxMarkDown(str) {
     var count = 0;
     var start = -1, end = -1;
     var operation = str;
-    while ((start = operation.search(/\{\[\{.*\}\]\}/)) >= 0) {
-        end = operation.search(/\}\]\}/);
+    while ((start = operation.search(/\{\[\{.*\}\]\}/ig)) >= 0) {
+        end = operation.search(/\}\]\}/ig);
         mathlist.push(operation.slice(start + 3, end));
-        operation = operation.replace(/\{\[\{.*\}\]\}/, "{^{" + count + "}^}");
+        operation = operation.replace(/\{\[\{.*?\}\]\}/, "{^{" + count + "}^}");
     }
     operation = converter.makeHtml(operation);
     for (let index = 0; index < mathlist.length; index++) {
