a42e44cb44b3b6bf7be32aa20527f08188483719
Felix <859317053@qq.com>
2021-07-01 10:09:55 +0800

    Add files via upload

--- blocks/pages_psyeco/contentStructure.js
+++ blocks/pages_psyeco/contentStructure.js
@@ -3,14 +3,19 @@ var contentArray = {
     "生活哲学": {
         "为什么要哲学?": "./pages_psyeco/life-psy/1.md",
         "心理学": {},
-        "论性格": {},
+        "论性格": {
+            "HSP高敏感人群": "./pages_psyeco/life-psy/personality/1.md"
+        },
         "论友情": {},
         "论爱情": {},
         "论事业": {
             "工作-心态-健康": "./pages_psyeco/life-psy/cause/1.md",
             "过度思考Overthink": "./pages_psyeco/life-psy/cause/2.md",
+            "不要犹豫,Just Do It": "./pages_psyeco/life-psy/cause/3.md"
         },
-        "论学习": {}
+        "论学习": {
+            "高效学习": ""
+        }
     },
     "经济学": {},
     "博弈论": {},
