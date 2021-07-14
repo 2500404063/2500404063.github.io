95abde585a188bee4620cc088a773a93ca919f48
Felix <859317053@qq.com>
2021-07-14 23:39:31 +0800

    Add files via upload

--- blocks/pages_ai/contentStructure.js
+++ blocks/pages_ai/contentStructure.js
@@ -1,10 +1,11 @@
 var contentArray = {
     "Default Page": "./pages_ai/default/default.md",
-    "Html": {
-        "Elements": {
-            "item1_1_1": "./pages_ai/hello/main.md"
-        },
-        "item1_1": "./pages_ai/hello/main.md",
+    "数学": {
+        "函数":"./pages_ai/math/1.md",
+        "泰勒Taylor":"./pages_ai/math/2.md",
+        "拉格朗日乘数法":"./pages_ai/math/3.md",
+        "正态分布":"./pages_ai/math/4.md",
+        "贝叶斯":"./pages_ai/math/5.md"
     },
     "list2": {
         "item2_1": "./pages_ai/hello/main.md",
