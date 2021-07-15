dcc3743e791a2521480384039d89bd67d49679e3
Felix <859317053@qq.com>
2021-07-15 12:50:44 +0800

    Add files via upload

--- blocks/pages_appdev/contentStructure.js
+++ blocks/pages_appdev/contentStructure.js
@@ -1,6 +1,12 @@
 var contentArray = {
     "Default Page": "./pages_appdev/default/default.md",
-    "CPP高级编程": {},
+    "CPP高级编程": {
+        '泛型': './pages_appdev/advanced_cpp/1.md',
+        'Using': './pages_appdev/advanced_cpp/2.md',
+        'Tuple元组': './pages_appdev/advanced_cpp/3.md',
+        '统一初始化': './pages_appdev/advanced_cpp/4.md',
+        'Lambda匿名函数': './pages_appdev/advanced_cpp/5.md',
+    },
     "线程": {
         "线程池": "./pages_appdev/thread/1.md"
     }
