7d5e133cb6da90a8d1016b801431064d51b3042c
Felix <859317053@qq.com>
2021-07-15 23:04:51 +0800

    Add files via upload

--- blocks/pages_appdev/contentStructure.js
+++ blocks/pages_appdev/contentStructure.js
@@ -6,6 +6,10 @@ var contentArray = {
         'Tuple元组': './pages_appdev/advanced_cpp/3.md',
         '统一初始化': './pages_appdev/advanced_cpp/4.md',
         'Lambda匿名函数': './pages_appdev/advanced_cpp/5.md',
+        'Constexpr': './pages_appdev/advanced_cpp/6.md',
+        '引用右值': './pages_appdev/advanced_cpp/7.md',
+        '智能指针': './pages_appdev/advanced_cpp/8.md',
+        '协程':'./pages_appdev/advanced_cpp/9.md',
     },
     "线程": {
         "线程池": "./pages_appdev/thread/1.md"
